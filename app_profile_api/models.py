62d86e6543e0148bcb2bc34170d461b759a83423
Endang Ismaya <endang.ismaya@gmail.com>
2023-01-11 21:53:43 +0000

    recreate app_profile_api

--- app_profile_api/models.py
+++ app_profile_api/models.py
@@ -1,9 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
 
-
-class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database model for users in the system"""
-
-    email = models.EmailField(max_length=255, unique=True)
+# Create your models here.
