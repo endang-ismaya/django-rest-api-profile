881bbc8600a3af7fadb0f512a06853c228229ee9
Endang Ismaya <endang.ismaya@gmail.com>
2023-01-11 21:43:08 +0000

    setup vscode on vagrant

--- app_profile_api/models.py
+++ app_profile_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
